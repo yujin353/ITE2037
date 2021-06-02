d81062d6aea589a7f596e8f7faa82a37d395a957
2020056353 <kimyujin0331@naver.com>
2021-06-02 15:56:29 +0900

    first commit

---
+++ lab12/Buffer.java
@@ -0,0 +1,38 @@
+package lab12;
+
+public class Buffer {
+	private int loc = 0;
+	private double[] data;
+
+	public Buffer(int size) {
+		data = new double[size];
+	}
+
+	public int getSize() {
+		return data.length;
+	}
+
+	synchronized void add(double toAdd) throws InterruptedException {
+		if (loc >= data.length) {
+			System.out.println("Buffer is full");
+			wait();
+		}
+		System.out.println("Adding item " + toAdd);
+		// System.out.flush();
+		data[loc++] = toAdd;
+		notifyAll();
+	}
+
+	synchronized double remove() throws InterruptedException {
+		if (loc == 0) {
+			System.out.println("buffer is empty");
+			wait();
+		}
+		double hold = data[--loc];
+		data[loc] = 0.0;
+		System.out.println("Removing item " + hold);
+		// System.out.flush();
+		notifyAll();
+		return hold;
+	}
+}
---
+++ lab12/ProdConTest.java
@@ -0,0 +1,10 @@
+package lab12;
+
+public class ProdConTest {
+
+	public static void main(String[] args) {
+		ProdConSelfTest a = new ProdConSelfTest();
+		a.startThread();
+	}
+
+}
